2dc8f5cde3223849e9e452c1e367e711e6be78d4
koder77 <jay-t@gmx.net>
2019-03-09 14:07:28 +0100

    "zerobuild force" now works too
    New: "zerobuild force" now forces to make all sources with "zerobuild.txt" makefile.

--- parser.c
+++ parser.c
@@ -56,7 +56,7 @@ char *fgets_uni (char *str, int len, FILE *fptr)
             default:
 				str[i] = ch;
 				i++;
-                
+
                 break;
         }
 
@@ -101,7 +101,7 @@ void convtabs (U1 *str)
 void remove_newline (U1 *str)
 {
     S2 line_len;
-    
+
     line_len = strlen (str) - 1;
     if (str[line_len] == '\n')
     {
@@ -112,13 +112,13 @@ void remove_newline (U1 *str)
 S2 search_char (U1 *str, U1 search_char, S2 search_start)
 {
     S2 i, str_len;
-    
+
     str_len = strlen (str);
     if (search_start >= str_len || search_start < 0)
     {
         return (-1);        // index out of range
     }
-    
+
     for (i = search_start; i < str_len; i++)
     {
         if (str[i] == search_char)
@@ -126,22 +126,22 @@ S2 search_char (U1 *str, U1 search_char, S2 search_start)
             return (i);     // return position of char in string
         }
     }
-    
+
     return (-1);    // char not found in string
 }
-    
+
 S2 get_assign (U1 *str, U1 *ret, S2 ret_size, S2 start)
 {
     S2 i, str_len, ret_ind = -1;
     U1 found_end = 0;
     U1 found_quote = 0;
-    
+
     str_len = strlen (str);
     if (start >= str_len || start < 0)
     {
         return (-1);        // start out of range
     }
-    
+
     i = start;
     while (found_end == 0)
     {
@@ -179,10 +179,10 @@ S2 get_assign (U1 *str, U1 *ret, S2 ret_size, S2 start)
                     printf ("get_assign: ERROR return buffer overflow!\n");
                     return (1);
                 }
-                
+
                 ret[ret_ind] = str[i];
             }
-            
+
             if (found_quote == 2)
             {
                 found_end = 1;
@@ -208,12 +208,12 @@ S2 get_assign (U1 *str, U1 *ret, S2 ret_size, S2 start)
                         printf ("get_assign: ERROR return buffer overflow!\n");
                         return (1);
                     }
-                
+
                     ret[ret_ind] = str[i];
                 }
             }
         }
-        
+
         if (i < str_len)
         {
             i++;
@@ -225,22 +225,22 @@ S2 get_assign (U1 *str, U1 *ret, S2 ret_size, S2 start)
     }
     ret_ind++;
     ret[ret_ind] = '\0';
-    
+
     return (0);
 }
- 
+
 get_assign_in_quote (U1 *str, U1 *ret, S2 ret_size, S2 start)
 {
 	S2 i, str_len, ret_ind = -1;
     U1 found_end = 0;
     U1 found_quote = 0;
-    
+
     str_len = strlen (str);
     if (start >= str_len || start < 0)
     {
         return (-1);        // start out of range
     }
-    
+
     i = start;
     while (found_end == 0)
     {
@@ -249,7 +249,7 @@ get_assign_in_quote (U1 *str, U1 *ret, S2 ret_size, S2 start)
 			if (found_quote == 0)
 			{
 				found_quote = 1;
-				
+
 				if (i < str_len)
 				{
 					i++;
@@ -263,8 +263,8 @@ get_assign_in_quote (U1 *str, U1 *ret, S2 ret_size, S2 start)
 			{
 				found_end = 1;
 			}
-		}	
-		
+		}
+
 		if (! found_end)
 		{
 			if (ret_ind < ret_size - 2)
@@ -276,9 +276,9 @@ get_assign_in_quote (U1 *str, U1 *ret, S2 ret_size, S2 start)
 				printf ("get_assign: ERROR return buffer overflow!\n");
 				return (1);
 			}
-			
+
 			ret[ret_ind] = str[i];
-			
+
 			if (i < str_len)
 			{
 				i++;
@@ -298,27 +298,27 @@ S2 parse_line (U1 *line)
     S2 line_len;
     S2 block_start = -1, block_end = -1, pos, search_start;
     char *ret;
-    
+
     U1 buf[4096];
-    
+
     U1 line_end = 0;
-        
+
     line_len = strlen (line);
-    
-    
-    // check if line begins with a # 
-    
+
+
+    // check if line begins with a #
+
     if (line[0] == '#')
     {
         return (0);     // line is a comment
     }
-    
-    
-    
+
+
+
     // not a comment, here we go:
-    
-    // check for type block 
-    
+
+    // check for type block
+
     search_start = 0;
     pos = search_char (line, BLOCK_OPEN, search_start);
     if (pos >= 0)
@@ -328,7 +328,7 @@ S2 parse_line (U1 *line)
         if (pos == -1)
         {
             // syntax error, no block close found
-            
+
             printf ("syntax ERROR no block close found!\n");
             return (1);
         }
@@ -340,15 +340,15 @@ S2 parse_line (U1 *line)
                 return (1);
             }
             else
-            {    
+            {
                 block_end = pos;
             }
         }
-        
+
         if (block_start >= 0 && block_end >= 0)
         {
             // found type block, do parse block
-            
+
             ret = strstr (line, TYPE_EXECUTABLE);
             if (ret != 0)
             {
@@ -357,25 +357,25 @@ S2 parse_line (U1 *line)
                     printf ("syntax ERROR: type must be one of executable OR library!\n");
                     return (1);
                 }
-                
+
                 parsed_line.type = EXECUTABLE;
             }
-            
+
             ret = strstr (line, TYPE_LIBRARY);
             if (ret != 0)
             {
                 // pos = ret - line;
-                
+
                 if (parsed_line.type == EXECUTABLE)
                 {
                     printf ("syntax ERROR: type must be one of executable OR library!\n");
                     return (1);
                 }
-                
+
                 parsed_line.type = LIBRARY;
             }
-            
-            
+
+
             ret = strstr (line, STATIC);
             if (ret != 0)
             {
@@ -384,29 +384,29 @@ S2 parse_line (U1 *line)
                     printf ("syntax ERROR: buildtype must be one of static OR shared!\n");
                     return (1);
                 }
-                
+
                 parsed_line.buildtype = BUILD_STATIC;
             }
-            
+
             ret = strstr (line, SHARED);
             if (ret != 0)
             {
                 // pos = ret - line;
-                
+
                 if (parsed_line.buildtype == BUILD_STATIC)
                 {
                     printf ("syntax ERROR: buildtype must be one of static OR shared!\n");
                     return (1);
                 }
-                
+
                 parsed_line.buildtype = BUILD_SHARED;
             }
-            
+
             ret = strstr (line, NAME);
             if (ret != 0)
             {
                 pos = (U1 *) ret - line;
-                
+
                 search_start = pos + 1;
                 pos = search_char (line, ASSIGN, search_start);
                 if (pos == -1)
@@ -414,13 +414,13 @@ S2 parse_line (U1 *line)
                     printf ("syntax ERROR: no assign found for name!\n");
                     return (1);
                 }
-                
+
                 if (get_assign (line, parsed_line.name, 4096, pos + 1) != 0)
                 {
                     printf ("syntax ERROR: can't get assign string!\n");
                     return (1);
                 }
-                
+
                 printf ("name = '%s'\n", parsed_line.name);
             }
         }
@@ -435,28 +435,28 @@ S2 parse_line (U1 *line)
                 // new assign, not a sources line anymore so switch flag off
                 sources_line = 0;
             }
-            
+
             if (includes_line >= 1)
             {
                 // // new assign, not a includes line anymore so switch flag off
                 includes_line = 0;
             }
         }
-            
-        
+
+
         // check assign
-    
+
         ret = strstr (line, SOURCES);
         if (ret != 0 || sources_line >= 1)
         {
             printf ("sources parser running...\n");
-            
+
             sources_line++;               // is source code line
-            
+
             if (sources_line == 1)
             {
                 pos = (U1 *) ret - line;
-                
+
                 search_start = pos + 1;
                 pos = search_char (line, ASSIGN, search_start);
                 if (pos == -1)
@@ -469,17 +469,17 @@ S2 parse_line (U1 *line)
             {
                 pos = -1;
             }
-                
+
             // sources = foo.c, bar.c, test.c
-                
-                
+
+
             while (line_end == 0)
             {
                 if (parsed_line.sources_ind < MAXSOURCES)
                 {
                     parsed_line.sources_ind++;
                 }
-                    
+
                 if (get_assign (line, parsed_line.sources[parsed_line.sources_ind], 4096, pos + 1) != 0)
                 {
                     if (sources_line == 1)
@@ -488,13 +488,13 @@ S2 parse_line (U1 *line)
                         return (1);
                     }
                 }
-                    
+
                 pos = search_char (line, COMMA, pos + 1);
                 if (pos == -1)
                 {
                     line_end = 1;
                 }
-                
+
                 if (parsed_line.sources[parsed_line.sources_ind][0] == '\0')
                 {
                     parsed_line.sources_ind--;
@@ -506,18 +506,18 @@ S2 parse_line (U1 *line)
                 }
             }
         }
-            
+
         ret = strstr (line, INCLUDES);
         if (ret != 0 || includes_line >= 1)
         {
             printf ("includes parser running...\n");
-            
+
             includes_line++;
-            
+
             if (includes_line == 1)
             {
                 pos = (U1 *) ret - line;
-                
+
                 search_start = pos + 1;
                 pos = search_char (line, ASSIGN, search_start);
                 if (pos == -1)
@@ -530,17 +530,17 @@ S2 parse_line (U1 *line)
             {
                 pos = -1;
             }
-                
+
             // sources = foo.c, bar.c, test.c
-                
-                
+
+
             while (line_end == 0)
             {
                 if (parsed_line.includes_ind < MAXINCLUDES)
                 {
                     parsed_line.includes_ind++;
                 }
-                    
+
                 if (get_assign (line, parsed_line.includes[parsed_line.includes_ind], 4096, pos + 1) != 0)
                 {
                     if (includes_line == 1)
@@ -549,13 +549,13 @@ S2 parse_line (U1 *line)
                         return (1);
                     }
                 }
-                    
+
                 pos = search_char (line, COMMA, pos + 1);
                 if (pos == -1)
                 {
                     line_end = 1;
                 }
-                
+
                 if (parsed_line.includes[parsed_line.includes_ind][0] == '\0')
                 {
                     parsed_line.includes_ind--;
@@ -567,18 +567,18 @@ S2 parse_line (U1 *line)
                 }
             }
         }
-        
+
         ret = strstr (line, OBJECTS);
         if (ret != 0 || objects_line >= 1)
         {
             printf ("objects parser running...\n");
-               
+
             objects_line++;
-            
+
             if (objects_line == 1)
             {
                 pos = (U1 *) ret - line;
-                
+
                 search_start = pos + 1;
                 pos = search_char (line, ASSIGN, search_start);
                 if (pos == -1)
@@ -591,17 +591,17 @@ S2 parse_line (U1 *line)
             {
                 pos = -1;
             }
-                
+
             // objects = foo.o, bar.o, test.o
-                
-                
+
+
             while (line_end == 0)
             {
                 if (parsed_line.objects_ind < MAXOBJECTS)
                 {
                     parsed_line.objects_ind++;
                 }
-                    
+
                 if (get_assign (line, parsed_line.objects[parsed_line.objects_ind], 4096, pos + 1) != 0)
                 {
                     if (objects_line == 1)
@@ -610,13 +610,13 @@ S2 parse_line (U1 *line)
                         return (1);
                     }
                 }
-                    
+
                 pos = search_char (line, COMMA, pos + 1);
                 if (pos == -1)
                 {
                     line_end = 1;
                 }
-                 
+
                 if (parsed_line.objects[parsed_line.objects_ind][0] == '\0')
                 {
                     parsed_line.objects_ind--;
@@ -628,13 +628,13 @@ S2 parse_line (U1 *line)
                 }
             }
         }
-        
-        
+
+
         ret = strstr (line, CFLAGS);
         if (ret != 0)
         {
             pos = (U1 *) ret - line;
-                
+
             search_start = pos + 1;
             pos = search_char (line, ASSIGN, search_start);
             if (pos == -1)
@@ -642,21 +642,21 @@ S2 parse_line (U1 *line)
                 printf ("syntax ERROR: no assign found for cflags!\n");
                 return (1);
             }
-                
+
             if (get_assign_in_quote (line, parsed_line.cflags, 4096, pos + 1) != 0)
             {
                 printf ("syntax ERROR: can't get assign string!\n");
                 return (1);
             }
-                
+
             printf ("cflags = '%s'\n", parsed_line.cflags);
         }
-        
+
         ret = strstr (line, CPLUSPLUSFLAGS);
         if (ret != 0)
         {
             pos = (U1 *) ret - line;
-                
+
             search_start = pos + 1;
             pos = search_char (line, ASSIGN, search_start);
             if (pos == -1)
@@ -664,21 +664,21 @@ S2 parse_line (U1 *line)
                 printf ("syntax ERROR: no assign found for c++flags!\n");
                 return (1);
             }
-                
+
             if (get_assign_in_quote (line, parsed_line.cplusplusflags, 4096, pos + 1) != 0)
             {
                 printf ("syntax ERROR: can't get assign string!\n");
                 return (1);
             }
-                
+
             printf ("c++flags = '%s'\n", parsed_line.cplusplusflags);
         }
-        
+
         ret = strstr (line, CCOMPILER);
         if (ret != 0)
         {
             pos = (U1 *) ret - line;
-                
+
             search_start = pos + 1;
             pos = search_char (line, ASSIGN, search_start);
             if (pos == -1)
@@ -686,21 +686,21 @@ S2 parse_line (U1 *line)
                 printf ("syntax ERROR: no assign found for ccompiler!\n");
                 return (1);
             }
-                
+
             if (get_assign (line, comp.c, 4096, pos + 1) != 0)
             {
                 printf ("syntax ERROR: can't get assign string!\n");
                 return (1);
             }
-                
+
             printf ("ccompiler = '%s'\n", comp.c);
         }
-        
+
         ret = strstr (line, CPLUSPLUSCOMPILER);
         if (ret != 0)
         {
             pos = (U1 *) ret - line;
-                
+
             search_start = pos + 1;
             pos = search_char (line, ASSIGN, search_start);
             if (pos == -1)
@@ -708,21 +708,21 @@ S2 parse_line (U1 *line)
                 printf ("syntax ERROR: no assign found for c++compiler!\n");
                 return (1);
             }
-                
+
             if (get_assign (line, comp.cplusplus, 4096, pos + 1) != 0)
             {
                 printf ("syntax ERROR: can't get assign string!\n");
                 return (1);
             }
-                
+
             printf ("c++compiler = '%s'\n", comp.cplusplus);
         }
-        
+
         ret = strstr (line, ARCHIVER);
         if (ret != 0)
         {
             pos = (U1 *) ret - line;
-                
+
             search_start = pos + 1;
             pos = search_char (line, ASSIGN, search_start);
             if (pos == -1)
@@ -730,21 +730,21 @@ S2 parse_line (U1 *line)
                 printf ("syntax ERROR: no assign found for archiver!\n");
                 return (1);
             }
-                
+
             if (get_assign (line, comp.archiver, 4096, pos + 1) != 0)
             {
                 printf ("syntax ERROR: can't get assign string!\n");
                 return (1);
             }
-                
+
             printf ("archiver = '%s'\n", comp.archiver);
         }
-        
+
         ret = strstr (line, AFLAGS);
         if (ret != 0)
         {
             pos = (U1 *) ret - line;
-                
+
             search_start = pos + 1;
             pos = search_char (line, ASSIGN, search_start);
             if (pos == -1)
@@ -752,21 +752,21 @@ S2 parse_line (U1 *line)
                 printf ("syntax ERROR: no assign found for aflags!\n");
                 return (1);
             }
-                
+
             if (get_assign (line, parsed_line.aflags, 4096, pos + 1) != 0)
             {
                 printf ("syntax ERROR: can't get assign string!\n");
                 return (1);
             }
-                
+
             printf ("aflags = '%s'\n", parsed_line.aflags);
         }
-        
+
         ret = strstr (line, LFLAGS);
         if (ret != 0)
         {
             pos = (U1 *) ret - line;
-                
+
             search_start = pos + 1;
             pos = search_char (line, ASSIGN, search_start);
             if (pos == -1)
@@ -774,33 +774,33 @@ S2 parse_line (U1 *line)
                 printf ("syntax ERROR: no assign found for lflags!\n");
                 return (1);
             }
-                
+
             if (get_assign_in_quote (line, parsed_line.lflags, 4096, pos + 1) != 0)
             {
                 printf ("syntax ERROR: can't get assign string!\n");
                 return (1);
             }
-                
+
             printf ("lflags = '%s'\n", parsed_line.lflags);
         }
     }
     return (0);
 }
- 
- 
+
+
 S2 parse_makefile (U1 *makefile)
 {
     FILE *fptr;
     U1 buf[MAXSTRLEN + 1];
     U1 *read;
     U1 ok = TRUE;
-    
+
     if ((fptr = fopen (makefile, "r")) == NULL)
     {
         printf ("ERROR: can't open makefile: '%s'\n", makefile);
         return (1);
     }
-    
+
     while (ok)
     {
         read = fgets_uni (buf, MAXSTRLEN, fptr);
@@ -822,20 +822,20 @@ S2 parse_makefile (U1 *makefile)
     fclose (fptr);
     return (0);
 }
-        
+
 
 int main (int ac, char *av[])
 {
     int ret;
     int force_build_all = 0;
-    
+
     parsed_line.sources_ind = -1;  // undefined index
     parsed_line.includes_ind = -1;
     parsed_line.objects_ind = -1;
-    
+
     parsed_line.type = NOTDEF;
     parsed_line.buildtype = NOTDEF;
-    
+
     strcpy (comp.c, "");
     strcpy (comp.cplusplus, "");
     strcpy (comp.archiver, "");
@@ -843,10 +843,12 @@ int main (int ac, char *av[])
     strcpy (parsed_line.cflags, "");
     strcpy (parsed_line.aflags, "");
     strcpy (parsed_line.lflags, "");
-    
+
     printf ("zerobuild V1.0.1\n");
-    
-    if (ac == 1)
+
+	printf ("DEBUG: ac: %i\n", ac);
+
+	if (ac == 1)
     {
         ret = parse_makefile ("zerobuild.txt");
     }
@@ -857,10 +859,20 @@ int main (int ac, char *av[])
             printf ("zerobuild <makefile> <force>\n");
             exit (1);
         }
-        
+
         if (ac == 2)
         {
-            ret = parse_makefile (av[1]);
+			printf ("DEBUG: av[1]: ''%s'\n", av[1]);
+			if (strcmp (av[1], "force") == 0)
+            {
+                printf ("FORCE FULL BUILD!\n");
+                force_build_all = 1;
+				ret = parse_makefile ("zerobuild.txt");
+            }
+			else
+			{
+            	ret = parse_makefile (av[1]);
+			}
         }
         if (ac == 3)
         {
@@ -878,17 +890,17 @@ int main (int ac, char *av[])
             ret = parse_makefile (av[1]);
         }
     }
-            
+
     if (ret == 1)
     {
         exit (1);
     }
-        
+
     ret = make (force_build_all);
     if (ret == 1)
     {
         exit (1);
     }
-    
+
     exit (0);       // no error
 }
